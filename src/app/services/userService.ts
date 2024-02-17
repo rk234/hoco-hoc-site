4324342f5f2d9c1ad183769502ff95a8182859eb
Ramy K <myramkadd@gmail.com>
2024-02-16 23:04:42 -0500

    initial user registration stuff

--- src/app/services/userService.ts
+++ src/app/services/userService.ts
@@ -1,13 +1,14 @@
-import { doc, getDoc } from "firebase/firestore";
+import { Timestamp, doc, getDoc, setDoc } from "firebase/firestore";
 import {auth, authProvider, db} from "../firebase/config"
 import { User, signInWithPopup, signOut } from "firebase/auth"
 
 type Profile = {
     uid: string
     email: string
-    creationDate: Date
+    creationDate: Timestamp
     displayName: string
-    scores: Map<string, number>
+    school: string
+    scores: any //look into a type for this later that works with firebase
     preferredLanguage: "python" | "cpp" | "java"
     admin: boolean
 }
@@ -18,9 +19,9 @@ export async function signInOrRegister() {
     console.log(user)
     
     if(await getUserData(user.uid)) {
-        console.log("User already registered!")
+        return true //Already registered
     } else {
-        console.log("User not already registered!")
+        return false //Not yet registered
     }
 }
 
@@ -28,8 +29,19 @@ export async function logout() {
     await signOut(auth)
 }
 
-export async function registerUser() {
-    //TODO
+export async function registerUserProfile(user: User, school: string, preferredLanguage: "python" | "cpp" | "java") {
+    const profile: Profile = {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        school: school,
+        preferredLanguage: preferredLanguage,
+        creationDate: Timestamp.fromDate(new Date()),
+        scores: {},
+        admin: false
+    }
+
+    await setDoc(doc(db, "users/"+profile.uid), profile)
 }
 
 export async function getUserData(uid: string) {
