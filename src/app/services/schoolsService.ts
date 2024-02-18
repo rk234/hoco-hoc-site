3f2526044d3868eaa3aa7976ada3e688ce48110c
Ramy K <myramkadd@gmail.com>
2024-02-17 23:30:10 -0500

    add other schools

--- src/app/services/schoolsService.ts
+++ src/app/services/schoolsService.ts
@@ -18,6 +18,46 @@ const schools: School[] = [
     {
         id: "marriots",
         name: "Marriots Ridge High School"
+    },
+    {
+        id: "atholton",
+        name: "Atholton High School"
+    },
+    {
+        id: "glenelg",
+        name: "Glenelg High School"
+    },
+    {
+        id: "guilford",
+        name: "Guilford Park High School"
+    },
+    {
+        id: "hammond",
+        name: "Hammond High School"
+    },
+    {
+        id: "howard",
+        name: "Howard High School"
+    },
+    {
+        id: "long-reach",
+        name: "Long Reach High School"
+    },
+    {
+        id: "hebron",
+        name: "Mt. Hebron High School"
+    },
+    {
+        id: "oakland",
+        name: "Oakland Mills High School",
+    },
+    {
+        id: "resevoir",
+        name: "Resevoir High School"
+    },
+    {
+        id: "wilde-lake",
+        name: "Wilde Lake High School"
     }
 ]
 
