e53925bfac7742c74936d8004edc1fdef3b0943a
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2024-02-27 15:54:11 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./components/hero/hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col bg-sky-950 overflow-y-auto">
+    <main className="flex flex-col bg-sky-950">
       <Hero />
     </main>
   );
