12ae70113fdf8da866503e9e1bcb81171222a61e
Ramy K <myramkadd@gmail.com>
2024-03-08 23:14:43 -0500

    article rendering improvements and housekeeping

--- src/app/components/article-renderer/articleRenderer.tsx
+++ src/app/components/article-renderer/articleRenderer.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import Markdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import TabContainer from "../../components/tab-container/tabContainer"
 import { Profile } from '@/app/services/userService';
+import {ExclamationTriangleIcon, InformationCircleIcon} from "@heroicons/react/24/solid"
+import MultiCodeView from '../multi-code-view/MultiCodeView';
 
 type Props = {
     markdown: string
@@ -13,37 +12,64 @@ type Props = {
 }
 
 export default function ArticleRenderer(props: Props) {
+    function blockStyles(type: string): string {
+        if(type == "warn") {
+            return "border-amber-500 bg-amber-600/30"
+        } else if(type == "note") {
+            return "border-sky-300 bg-sky-300/30"
+        } else {
+            return ""
+        }
+    }
+
     return <Markdown
         className={`prose prose-slate leading-snug my-4 prose-invert 
             prose-headings:mt-5 prose-headings:mb-2 prose-ul:mt-0 
             prose-pre:bg-transparent prose-pre:p-0 prose-li:my-1 ${props.className}`}
         components={{
-        code(code_props) {
-            const { children, className, node, ...rest } = code_props;
-            //console.log(node)
-            let langs = className.split('-')[1].split(',');
-            let examples = (children as string).split('\n%%\n');
+            blockquote(quote_props) {
+                const { children, className, node, ...rest } = quote_props;
+                let type: string;
 
-            let renderResult = [];
-            for (var i = 0; i < langs.length; i++) {
-            renderResult.push(
-                <SyntaxHighlighter
-                PreTag="div"
-                language={langs[i]}
-                style={{...theme}}
-                codeTagProps={{className: "font-mono"}}
-                showLineNumbers={true}
-                showInlineLineNumbers={true}
-                wrapLongLines={false}
-                >
-                    {examples[i].trim()}
-                </SyntaxHighlighter>
-            );
+                let content =children[1] ?  children[1].props.children as string : ""
+
+                if(content) {
+                    if(content.trim().toLowerCase().startsWith(":note:")) {
+                        type = "note"
+                        content = content.replace(":note:", "")
+                    } else if(content.trim().toLowerCase().startsWith(":warn:")) {
+                        type = "warn"
+                        content = content.replace(":warn:", "")
+                    } else {
+                        type = "block"
+                    }
+                }
+
+                const Icon = () => {
+                    if(type == "note") return <InformationCircleIcon className='w-7 h-7' />
+                    else if(type == "warn") return <ExclamationTriangleIcon className='w-7 h-7' />
+                    else return ""
+                }
+
+                return <div className={`my-2 flex flex-row items-center gap-2 bg-slate-800 border rounded border-slate-700 border-l-8 p-2 ${blockStyles(type)}`}>
+                    <div className='self-start'>{Icon()}</div>
+                    <p className='m-0'>{content}</p>
+                </div>
+            },
+            code(code_props) {
+                const { children, className, node, ...rest } = code_props;
+                let langs = className ? className.split('-')[1].split(',') : [];
+                let examples = children ? (children as string).split('\n%%\n') : [];
+                let out: string
+                if(langs[langs.length-1] == "out" && examples.length==langs.length) {
+                    langs.pop()
+                    out = examples.pop()
+                }
+
+                return (
+                    <MultiCodeView selected={props.profile ? props.profile.preferredLanguage : undefined} langs={langs} codeSamples={examples} output={out} />
+                );
             }
-            return (
-                <TabContainer selected={props.profile ? props.profile.preferredLanguage : undefined} langs={langs} components={renderResult} />
-            );
-        }
         }}
     >
         {props.markdown}
---
+++ src/app/components/multi-code-view/MultiCodeView.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { useState } from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+type Props = {
+    className?: string
+    langs: string[]
+    codeSamples: string[]
+    output?: string
+    selected?: string
+}
+
+export default function MultiCodeView(props: Props) {
+    let [idx, setIdx] = useState(props.selected ? findIndex() : 0);
+
+    function findIndex() {
+        const i = props.langs.indexOf(props.selected)
+        return i == -1 ? 0 : i;
+    }
+    return <div className={`${props.className}`}>
+        {props.langs.map((lang, i) => (
+            <button className={`${(i == idx) ? "btn-primary" : "btn-secondary"} p-1 mr-2 text-xs`} key={i} onClick={() => setIdx(i)}>{lang.toUpperCase()}</button>
+        ))}
+        <div className="m-0">
+            <SyntaxHighlighter
+                PreTag="div"
+                language={props.langs[idx]}
+                style={{ ...theme }}
+                codeTagProps={{ className: "font-mono" }}
+                showLineNumbers={true}
+                showInlineLineNumbers={true}
+                wrapLongLines={false}
+            >
+                { (idx < props.codeSamples.length) && props.codeSamples[idx].trim()}
+            </SyntaxHighlighter>
+        </div>
+        {props.output && <div>
+            <p className="m-0 text-slate-300 p-2 rounded bg-[#282a36] uppercase italic font-bold text-sm">Output:</p>
+            <SyntaxHighlighter
+                PreTag="div"
+                language={"text"}
+                style={{ ...theme }}
+                codeTagProps={{ className: "font-mono" }}
+                showLineNumbers={true}
+                showInlineLineNumbers={true}
+                wrapLongLines={false}
+            >
+                {props.output.trim()}
+            </SyntaxHighlighter>
+        </div>}
+    </div>
+}
