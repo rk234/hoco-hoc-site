d415bf5a456ce9d042444eaa37052ca13681fd7f
Ramy K <myramkadd@gmail.com>
2024-03-16 23:39:33 -0400

    more data model updates to reduce requests

--- src/app/services/quizService.ts
+++ src/app/services/quizService.ts
@@ -13,20 +13,20 @@ export type Question = {
 }
 
 export async function getQuiz(id: string): Promise<Quiz> {
-    let ref = doc(db, "quizzes/"+id)
+    let ref = doc(db, "quizzes/" + id)
     let quiz = (await getDoc(ref)).data() as Quiz
     return quiz
 }
 
 export async function getQuizAnswers(quiz_id: string) {
-    let ref = doc(db, "quiz-ans/"+quiz_id)
+    let ref = doc(db, "quiz-ans/" + quiz_id)
     let answers = (await getDoc(ref)).data().answers as number[]
     return answers
 }
 
 //answers is an array of indexes representing the correct answers for each of the question object's options
 //returns a boolean representing whether correct/incorrect based on response from cloud fn
-export async function checkAnswers(quiz_id: string, answers: number[]) : Promise<boolean> {
+export async function checkAnswers(quiz_id: string, answers: number[]): Promise<boolean> {
     //todo call cloud fn
 }
 
@@ -34,8 +34,9 @@ export async function checkAnswers(quiz_id: string, answers: number[]) : Promise
 export async function createQuiz(quiz: Quiz, answers: number[]) {
     let quizRef = doc(db, "quizzes/" + quiz.id);
     let ansRef = doc(db, "quiz-ans/" + quiz.id);
-    await setDoc(quizRef, quiz, {merge: true})
+    await setDoc(quizRef, quiz, { merge: true })
     await setDoc(ansRef, {
-                    answers: answers
-                }, {merge: true})
+        points: quiz.points,
+        answers: answers
+    }, { merge: true })
 }
