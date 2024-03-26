5a6c994d48bd7741f816e26d114b81637aab6a08
arif24v <135023226+arif24v@users.noreply.github.com>
2024-03-25 23:12:03 -0400

    Update page.tsx

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -59,6 +59,8 @@ export default function Articles() {
         return hydrated
     }
 
+    
+
     return <main className="p-4">
         {error ? <ModalContainer>
             <Modal className="flex flex-col">
@@ -73,7 +75,6 @@ export default function Articles() {
         </p>
         <div className="">
             <div id="buttons" className="flex justify-center gap-2 bg-sky-900 sm:w-1/2 lg:w-1/3 m-3 p-3 mx-auto rounded-lg">
-
                 <div className="flex flex-col items-center mr-2">
                     <div className="rounded-full bg-slate-300 w-16 h-16 flex items-center justify-center text-center hover:cursor-pointer hover:-translate-y-1.5 ease-in-out duration-300">
                         <span className="text-black">##</span>
@@ -101,9 +102,6 @@ export default function Articles() {
                     </div>
                     <span className="text-center hover:cursor-pointer">Section 4</span>
                 </div>
-
-                
-
             </div>
             <div>
                 <div className="flex justify-center mt-4"> 
@@ -115,72 +113,63 @@ export default function Articles() {
                     <p className="sm:text-right text-center"> 0 / 273 tasks completed</p>
                 </div>
             </div>
-
         </div>
 
         <ul className="list-disc ml-4 mt-5">
         {sections.map((section, index) => {
-    return (
-    <div>
-      <div key={index} className="md:flex gap-4 justify-center "
-      onClick={() => setShowContent(!showContent)}>
-        <div className="hover:-translate-y-2 bg-blue-950 cursor-pointer p-4 rounded-lg border mb-4 md:w-1/2 ease-in-out duration-300 hover:shadow-xl hover:shadow-indigo-500/50">
-          <p className="font-bold font-['Menlo']">{section.title}</p>
-          <p className="pt-4 font-mono">{section.description}</p>
-          <div className="flex">
-            <Link className="link no-underline pt-3" href={''}>
-              Learn More
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 inline-block"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-              </svg>
-            </Link>
-          </div>
-        </div>
-        </div>
-        {showContent && (
-        <div className={`md:flex gap-4 justify-center ${showContent ? 'animate-slideout show' : 'hidden'}`}>
-    <div className="flex justify-left hover:-translate-y-2 bg-indigo-900 cursor-pointer p-4 rounded-lg border md:w-1/2 mb-4 ease-in-out duration-300 hover:shadow-xl hover:shadow-indigo-500/50">
-        <ul className="justify-left">
-            <li>
-                <div className="flex flex-row ml-3 justify-left">
-                    <div className="text-xl font-mono">Hello World!</div>
-                    <div className="ml-3 pl-2 pr-2 h-1/6 bg-cyan-500 rounded-lg">
-                        To-Do
+            return (
+                <div>
+                    <div key={index} className="md:flex gap-4 justify-center "
+                    onClick={() => setShowContent(!showContent)}>
+                        <div className="hover:-translate-y-2 bg-blue-950 cursor-pointer p-4 rounded-lg border mb-4 md:w-1/2 ease-in-out duration-300 hover:shadow-xl hover:shadow-indigo-500/50">
+                            <p className="font-bold font-['Menlo']">{section.title}</p>
+                            <p className="pt-4 font-mono">{section.description}</p>
+                            <div className="flex">
+                                <Link className="link no-underline pt-3" href={''}>
+                                    Learn More
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="w-6 h-6 inline-block"
+                                    >
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                    </svg>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
+                    {showContent && 
+                        section.articles.map((article, articleIndex) => (
+                            <div className={`md:flex gap-4 justify-center ${showContent ? 'animate-slideout show' : 'hidden'}`}>
+                                <div className="flex justify-left hover:-translate-y-2 bg-indigo-900 cursor-pointer p-4 rounded-lg border md:w-1/2 mb-4 ease-in-out duration-300 hover:shadow-xl hover:shadow-indigo-500/50">
+                                    <ul className="justify-left">
+                                        <li>
+                                            <div className="flex flex-row ml-3 justify-left">
+                                                <div className="text-xl font-mono">{article.id}</div>
+                                                <div className="ml-3 pl-2 pr-2 h-1/6 bg-cyan-500 rounded-lg">
+                                                    To-Do
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-row mt-2 ">
+                                            {article.tags.map((tag, tagIndex) => (
+                                                <div className="ml-3 pl-2 pr-2 bg-cyan-500 rounded-lg"> {tag} </div>    
+                                            ))}
+                                            </div>
+                                            <div className="ml-3 mt-2"> 
+                                                <p className="text-lg text-slate-400"> {article.description}</p>
+                                            </div>
+                                            
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        ))}
                 </div>
-                <div className="flex flex-row mt-2 ">
-                    <div className="ml-3 pl-2 pr-2 bg-cyan-500 rounded-lg"> 4pts </div>
-                    <div className="ml-3 pl-2 pr-2 bg-slate-500 rounded-lg"> Python </div>
-                    <div className="ml-3 pl-2 pr-2 bg-slate-500 rounded-lg"> Java </div>
-                    <div className="ml-3 pl-2 pr-2 bg-slate-500 rounded-lg"> C++ </div>
-                    <div className="ml-3 pl-2 pr-2 bg-slate-500 rounded-lg"> Syntax </div>
-                </div>
-                <div className="ml-3 mt-2"> 
-                    <p className="text-lg text-slate-400"> The start of a journey</p>
-                </div>
-            </li>
-        </ul>
-    </div>
-</div>
-        )}
-
-
-        
-      </div>
-      
-    );
-  }
-)}
-
-
-
+            );
+        })}
         </ul>
     </main>
 }
