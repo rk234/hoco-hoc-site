b1f20c7bef34d201a1b902b9abbe9fd1f60e9010
arif24v <135023226+arif24v@users.noreply.github.com>
2024-04-16 08:54:57 -0400

    Update prizes.tsx

--- src/app/components/prizes/prizes.tsx
+++ src/app/components/prizes/prizes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Prizes() {
     return (
         <div>
-            <div className="flex flex-col items-center bg-gradient-to-br from-black via-80% via-indigo-950 to-indigo-950 md:py-32 py-20">
+            <div className="p-8 flex flex-col items-center bg-gradient-to-br from-black via-80% via-indigo-950 to-indigo-950 md:py-32 py-20">
                 <div className="flex flex-col max-w-screen-xl items-start w-full">
                     <div className="w-full gap-12 flex flex-col max-w-3xl">
                         <div className="flex-1">
@@ -12,7 +12,7 @@ export default function Prizes() {
                     </div>
                 </div>
                 <div className="flex flex-col max-w-screen-xl items-start w-full">
-                    <div className="w-full gap-12 flex flex-row ">
+                    <div className="w-full gap-12 flex md:flex-col lg:flex-row ">
                         <div className="flex-1 pt-8">
                             <h2 className="text-white text-3xl text-center bg-sky-900 rounded-t-lg p-4">Individual Prizes</h2>
                             <ul className="rounded-b-xl bg-sky-800 p-4 pl-8 list-disc">
@@ -21,7 +21,7 @@ export default function Prizes() {
                                 <li>4x Art of Problem Solving gift cards ($25)</li>
                             </ul>
                         </div>
-                        <div className="flex-1 p-8">
+                        <div className="flex-1 pt-8">
                             <h2 className="text-white text-3xl text-center bg-sky-900 rounded-t-lg p-4">School Prizes</h2>
                             <ul className="rounded-b-xl bg-sky-800 p-4 pl-8 list-disc">
                                 <li>2x Razer Gaming keyboards ($180)</li>
