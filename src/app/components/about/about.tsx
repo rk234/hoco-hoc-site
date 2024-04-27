7e30a5b2e0e90cd16f957161d87b0a664e6bcd6d
Nicole Luo <nicoleluo@Nicoles-Air.home>
2024-04-26 22:55:51 -0400

    spacing tweaks and updated FAQ

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -31,19 +31,21 @@ export default function AboutSection() {
     return (
         <div className="px-8 md:px-0 lg:px-12 mb-20 text-center md:text-left">
             <h3 className="font-mono mt-16 ml-2 mr-2 lg:ml-25 lg:mr-25 text-sky-300 font-bold bg-gradient-to-b from-sky-300 to-sky-500 text-transparen font-bold text-transparent bg-clip-text text-5xl md:text-6xl mb-8 text-center leading-tight">About Us</h3>
-            <p className="md:px-12 mb-10 md:mb-10 sm:text-lg md:text-xl font-sans flex flex-row mt-8 gap-2">
+            <p className="md:px-12 mb-20 md:mb-10 sm:text-lg md:text-xl font-sans flex flex-row mt-8 gap-2">
                 The Hour of Code started as a one-hour introduction to computer science, designed to demystify "code", to show that anybody can learn the basics, and to broaden participation in the field of computer science. It has since become a worldwide effort to celebrate computer science, starting with 1-hour coding activities but expanding to all sorts of community efforts. Howard County's version of Hour of Code is made by students, for students.
             </p>
 
-            <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:ml-10 md:mr-10 mb-20">
+            <div className="flex flex-col space-y-4 md:space-y-16 lg:space-y-4 md:flex-row md:space-x-4 md:ml-10 md:mr-10 mb-16 md:mb-20">
                 <div className="md:w-64 flex-grow mb-8 md:mb-0">
                     <h3 className="sm:mt-20 md:mt-10 lg:mt-20 font-mono text-4xl md:text-5xl bg-gradient-to-r from-sky-300 to-sky-500 text-transparent bg-clip-text font-bold">Compete For Glory!</h3>
                     <p className="font-sans mt-10 mb-10 md:mr-10 text-l md:text-xl">We aim to provide aspiring computer scientists with an opportunity for skill development and friendly competition. Visit the leaderboard to check out how your high school is doing! All Howard County schools and corresponding points will be displayed here.</p>
                     <Link href={"/leaderboard"} className="btn-secondary text-lg">View Leaderboard</Link>
                 </div>
+               
                 <div className="flex-grow w-full h-full md:w-60 lg:w-0 text-slate-300 bg-gray-900 rounded-md border-gray-800 border-2 overflow-hidden">
-                    <Leaderboard schools={dummySchools} />
+                <Leaderboard schools={dummySchools} />
                 </div>
+               
             </div>
 
 
@@ -77,11 +79,11 @@ export default function AboutSection() {
 
             <div className="flex flex-col md:flex-row md:ml-8 md:mr-8 lg:ml-10 lg:mr-10 sm:mt-10 md:mt-0 lg:mt-10">
                 <div className="flex-grow max-w-2xl font-mono">
-                    <h3 className="mt-20 font-mono leading-tight text-4xl md:text-5xl bg-gradient-to-r from-sky-300 to-sky-500 text-transparent bg-clip-text font-bold">Track Your Progress!</h3>
-                    <p className="font-sans mt-10 mb-10 md:mr-5 lg:mr-10 text-l md:text-xl">Complete quizzes to earn points! You'll be able to see how you progress through four sections: Fundamentals, Data Structures & Algorithms, Web Development, and Machine Learning.</p>
+                    <h3 className="md:mr-10 mt-20 font-mono leading-tight text-4xl md:text-5xl bg-gradient-to-r from-sky-300 to-sky-500 text-transparent bg-clip-text font-bold">Track Your Progress!</h3>
+                    <p className="font-sans mt-10 mb-10 md:mr-10 text-l md:text-xl">Complete quizzes to earn points! You'll be able to see how you progress through four sections: Fundamentals, Data Structures & Algorithms, Web Development, and Machine Learning.</p>
                 </div>
                 <div className="flex-grow-2 justify-center md:mt-8">
-                    <div className="mt-4 md:mt-28 lg:mt-4 relative h-68 lg:h-60 bg-gray-800 rounded-lg p-4 flex flex-wrap md:flex-nowrap justify-center items-center">
+                    <div className="mt-4 md:mt-36 lg:mt-4 relative h-68 lg:h-60 bg-gray-800 rounded-lg p-4 flex flex-wrap md:flex-nowrap justify-center items-center">
                         {progressData.map((item, index) => (
                             <div key={index} className="text-center py-2 md:py-0 px-4 md:px-1">
                                 {/* Progress bar */}
