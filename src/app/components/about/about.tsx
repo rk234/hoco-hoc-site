e1bdd628a9cb616da320610a2f7ded2687db9545
Ramy K <myramkadd@gmail.com>
2024-05-24 23:32:56 -0400

    fix jumpy layout

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -10,7 +10,7 @@ export default function AboutSection() {
         "CS",
         "Algorithms",
         "Web Dev",
-        "Machine Learning",
+        "ML",
         "Python",
         "Java",
         "C++"
